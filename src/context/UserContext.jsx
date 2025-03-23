64a3f8c5b877116be2c777b0ff414744d5ded23c
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-03-24 02:20:25 +0600

    Updated UserContext

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,9 +1,16 @@
+import { createContext } from "react"
 
+export const datacontext = createContext()
 
-const UserContext = ({children}) => {
+const UserContext = ({ children }) => {
+  let value = {
+    
+  }
   return (
     <div>
+      <datacontext.Provider value={value}>
         {children}
+      </datacontext.Provider>
     </div>
   )
 }
